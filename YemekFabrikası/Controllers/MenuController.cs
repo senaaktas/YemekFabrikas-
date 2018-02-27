9a0fa9b9bef51be61bb91b0ed400892929520a1f
Sena Aktaş <senaaktas93@gmail.com>
2018-02-27 16:14:45 +0300

    Template Giydirildi.

---
+++ YemekFabrikası/Controllers/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YemekFabrikası.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
