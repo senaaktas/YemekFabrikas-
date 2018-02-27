83a6fa03e19c2bf419158f47b64af76daa5ff5b1
Sena Aktaş <senaaktas93@gmail.com>
2018-02-27 15:07:25 +0300

    YemekFabrikası Projesi

---
+++ YemekFabrikası/Controllers/GirisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YemekFabrikası.Controllers
+{
+    public class GirisController : Controller
+    {
+        // GET: Giris
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
